1f314fcd203f8f556aa5ee30af7a312a482f3db0
Jefferson <jpo.pereira@hotmail.com>
2021-03-08 14:37:33 -0300

    TESTE

--- src/main/java/com/houarizegai/calculator/Calculator.java
+++ src/main/java/com/houarizegai/calculator/Calculator.java
@@ -15,7 +15,7 @@ public class Calculator {
     private static final int WINDOW_WIDTH = 410;
     private static final int WINDOW_HEIGHT = 600;
     private static final int BUTTON_WIDTH = 80;
-    private static final int BUTTON_HEIGHT = 70;
+    private static final int BUTTON_HEIGHT = 80;
     private static final int MARGIN_X = 20;
     private static final int MARGIN_Y = 60;
 
